584042bc3360a86d881ad56e2e3e0a2ab8b1b6f2
jithish-sekar <jithusekar@gmail.com>
2020-05-25 07:54:03 +0530

    ecm

--- src/components/product.js
+++ src/components/product.js
@@ -6,17 +6,28 @@ class Product extends Component {
     render() {
         const { id, title, img, price, inCart } = this.props.product;
         return (
-            <React.Fragment >
+            <React.Fragment>
                 <div className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-                <div className="card">
-                <div className="img-container p-6"
-                    onClick={console.log("you clicked the image container")}>
-                    <Link to="details">
-                        <img src={img} className="productimg" />
-                    </Link>
-                </div>
+                    <div className="card">
+                     <div className="img-container p-7"
+                            onClick={() => console.log("you clicked the image container")}>
+                            <Link to="details">
+                                <img src={img} className="productimg" />
+                            </Link>
+                        <button className='buttoncart' disabled={inCart ? true : false} onClick={()=>console.log('added to cart')}>
+                                {inCart ? (<p>Incart</p>) : (
+                                    <i className='fa fa-cart-plus' />)}
+                        </button>
+                     </div>
+                        <div className="card-ftr">
+                            <p className="align-self-center mb-0">{title}</p>
+                            <h5 className="price">
+                            <span className="mr-1">₹</span>
+                            {price}
+                            </h5>       
+                        </div>
                     </div>
-            </div >
+                </div >
             </React.Fragment >
         )
     }
